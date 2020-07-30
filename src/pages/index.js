352cb3a62a5e1df8a48eecd2f8648346760e94ed
Auxane Nitharum <auxane.nith@protonmail.com>
2020-07-30 15:58:07 +0200

    Image fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,33 +7,43 @@ import SEO from "../components/seo"
 import MonkeyWrapper from '../components/threeMonkeySpin'
 import styled from 'styled-components'
 
-const IndexWrapper = styled.div`
-	position: absolute;
-	left: 0px;
-	right: 0px;
-	top: 0px;
-	bottom: 0px;
-  canvas{
-    width: 100%;
-    height: 100%;
-    display:block;
-  }
-`
+  const IndexWrapper = styled.div`
+    position: absolute;
+    left: 0px;
+    right: 0px;
+    top: 0px;
+    bottom: 0px;
+    canvas{
+      width: 100%;
+      height: 100%;
+      display:block;
+    }
+  `
+  const ImageWrapper = styled.div`
+        width: 60vw;
+        min-width:400px;
+        max-width:600px;
+        margin: 0;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+
+        /* @media screen AND (max-width:1024px){
+          position:fixed;
+          bottom:0;
+          z-index:99;
+        } */
+  `
+
 const IndexPage = () => (
   <Layout>
     <IndexWrapper>
       <SEO title="Home" />
       <MonkeyWrapper/>
-      <div style={{
-        width:'600px',
-        margin: '0',
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)'
-         }}>
+      <ImageWrapper>
         <WAOrgBase />
-      </div>
+      </ImageWrapper>
     </IndexWrapper>
   </Layout>
 )
