06f5ac7d90aad054308f2b0d03f0191a8a841f91
Auxane Nitharum <auxane.nith@protonmail.com>
2020-07-30 10:54:29 +0200

    ComingSoon component fix

--- src/pages/templates.js
+++ src/pages/templates.js
@@ -6,7 +6,7 @@ import styled from 'styled-components'
 
 import PageTitle from '../components/pageTitle';
 import TemplatesGallery from '../components/templatesGallery'
-import ComingSoon from '../components/ComingSoon'
+import ComingSoon from '../components/comingSoon'
 
 const VerticalAlign = styled.div`
   width:100%;
